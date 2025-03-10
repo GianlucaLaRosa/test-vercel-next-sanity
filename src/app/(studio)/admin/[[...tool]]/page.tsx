aa7476b97aed6b36f3913ac28215ea38c897ec8a
Gianluca La Rosa <gianluca_larosa@hotmail.com>
2025-03-10 11:19:47 +0100

    chore: force dynamic

--- src/app/(studio)/admin/[[...tool]]/page.tsx
+++ src/app/(studio)/admin/[[...tool]]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { NextStudio } from "next-sanity/studio";
-import config from "../../../../../sanity.config";
+import config from "sanity.config";
+
+export const dynamic = 'force-static'
 
 export default function Admin() {
   return (
